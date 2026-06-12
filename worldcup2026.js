98c23b6e813b3cd2da1b40fd1cec2951db522cdd
hunglehien1012 <hunglehien1012@gmail.com>
2026-06-12 16:37:58 +0700

    Update worldcup2026.js

--- worldcup2026.js
+++ worldcup2026.js
@@ -1,4 +1,4 @@
-      var SVG_SUN =
+var SVG_SUN =
         '<path d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"/><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"/>';
       var SVG_MOON =
         '<path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454l0 .008"/>';
@@ -1426,7 +1426,7 @@
 
       async function fetchCountOnly() {
         // Chỉ đọc, không tăng (dùng endpoint /info hoặc /up nhưng đọc count hiện tại)
-        var r = await fetch('https://api.counterapi.dev/v1/hunglehien1012/worldcup2026');
+        var r = await fetch('https://api.counterapi.dev/v1/hunglehien1012/worldcup2026/');
         if (!r.ok) throw new Error();
         return (await r.json()).count || 0;
       }
